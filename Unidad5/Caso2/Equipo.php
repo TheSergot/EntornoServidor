1ba18f52ef164e48d897016cbd8a9533800e0509
TheSergot <70240316+TheSergot@users.noreply.github.com>
2021-12-05 22:55:18 +0100

    Add files via upload

--- Unidad5/Caso2/Equipo.php
+++ Unidad5/Caso2/Equipo.php
@@ -7,6 +7,7 @@
 </head>
 <body>
 	<?php
+//Creamos la clase equipo, con el atributo jugadores como array, un constructor vacío y la función getter correspondiente	
 		class Equipo{
 			private array $jugadores;
 
@@ -16,15 +17,15 @@
 			public function getJugadores(){
 				return $this->jugadores;
 			}
-
+//Creamos la función addJug que nos permitirá añadir jugadores al array y les asignará un número de jugador
 			public function addJug(Jugador $jugador){
 				$this->jugadores[$jugador->getNumJug()] = $jugador;
 			}
-
+//Con la función getJug podemos acceder a un jugador en concreto por su número de dorsal
 			public function getJug(int $numJugador): Jugador{
 				return $this->jugadores[$numJugador];
 			}
-
+//Con la función getTotal obtendremos el total de puntos del equipo, recorriendo el array de jugadores y sumando los puntos de cada uno.
 			public function getTotal(): int{
 				$total = 0;
 				foreach ($this->jugadores as $value) {
